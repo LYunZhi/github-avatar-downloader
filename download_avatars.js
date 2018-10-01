798a43d8481ed86941efe8a7c0126ebfbbd2a981
Jim <yjzlin@gmail.com>
2018-10-01 20:04:13 +0000

    Added secret file

--- download_avatars.js
+++ download_avatars.js
@@ -1,9 +1,19 @@
 var request = require('request')
+var githubToken = require('./secrets.js')
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': githubToken
+    }
 
+  request(url, function(err, res, body) {
+    cb(err, body)
+  })
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
